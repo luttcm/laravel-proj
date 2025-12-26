4048b8ff96115916d374ce1d690cb34fac330836
Sergey Shemyatenkov <s.shemyatenkov@itgrade.ru>
2025-12-26 12:47:24 +0300

    fix: visual

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Calculator')</title>
+    <link rel="stylesheet" href="/app/public/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
